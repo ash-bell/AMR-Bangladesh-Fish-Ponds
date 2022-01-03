f5ab3d5e59247b6732f0a3d13d942741ed1006da
Ashley <44369148+ash-bell@users.noreply.github.com>
2022-01-03 14:02:48 +0000

    Create 02_ARG_search_RGI.sh

---
+++ 02_ARG_search_RGI.sh
@@ -0,0 +1,13 @@
+# Resistance Gene Identifier version 5.2.1
+
+rgi main -i REPLACE_ME/REPLACE_ME_scaffolds.fasta \
+-o REPLACE_ME/REPLACE_ME_RGI_BLAST.txt \
+-t contig \
+-a BLAST \
+-n 16 \
+--exclude_nudge \
+--clean \
+--low_quality \
+-d NA \
+--split_prodigal_job \
+--exclude_nudge
